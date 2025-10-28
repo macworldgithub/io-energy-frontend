b2e23d13d617565b296527fea154bbde255c281f
jawwadkareem <karimjawwad09@gmail.com>
2025-10-28 17:15:19 +0500

    ..

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
 // export const SERVER_URL = "https://www.widget.moving7.com";
 // export const SERVER_URL = "http://127.0.0.1:8000";
-// export const SERVER_URL = "http://localhost:7008";
-export const SERVER_URL = "https://io.omnisuiteai.com";
+export const SERVER_URL = "http://localhost:7008";
+// export const SERVER_URL = "https://io.omnisuiteai.com";
